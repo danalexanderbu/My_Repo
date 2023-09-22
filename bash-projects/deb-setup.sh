b72834a3f743af2db7211db4a3876814388981ab
Daniel Burke <daniel.burke.13@us.af.mil>
2023-09-21 23:14:34 -0500

    functional

--- bash-projects/deb-setup.sh
+++ bash-projects/deb-setup.sh
@@ -94,6 +94,12 @@ sudo apt install firefox-mozilla-build -y
 ### CAC ###
 
 
+### Kubernetes ###
+
+
+### Steam ###
+
+
 ### Brave Browser ###
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
