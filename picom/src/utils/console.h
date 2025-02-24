f367ca95a35c06be87e98c1324d98c658400fc55
dburke <danalexanderbu@gmail.com>
2025-02-23 18:38:18 -0600

    lets gooo

--- picom/src/utils/console.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-/// Generate ANSI escape code
-#define ANSI(x) "\033[" x "m"
-/// Create a string that will print `str` in bold when output to terminal
-#define BOLD(str) "\033[1m" str "\033[0m"
